302e649df6b06098ff0784a16a534e8de46547a0
hase111111 <hasehasehase61@gmail.com>
2025-04-22 16:27:18 +0900

    Add: ゲーム用の型を追加

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,38 @@
 
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 
 import WindowSetting
-import Type
+import BreakOut.Type
 
-sampleInit :: Int
-sampleInit = 0
 
-sampleDraw :: Int -> Picture
-sampleDraw n = Translate (-200) 0 $ Scale 0.5 0.5 $ Color (makeColor 1 0 0 1) $ Text (show (n `div` 60))
+sampleInit :: GameField
+sampleInit = GameField
+    { fps = 60
+    , scene = TitleDetails 0 "Title"
+    }
 
-sampleEventHandler :: Event -> Int -> Int
+sampleDraw :: GameField -> Picture
+sampleDraw GameField{..} = 
+    case scene of
+        TitleDetails _ _ -> 
+            Translate (-100) 0 $ Scale 0.1 0.1 $ Text $ show fps
+        GameDetails _ game -> 
+            Translate (-100) 0 $ Scale 0.1 0.1 $ Text game
+        GameOverDetails _ gameOver -> 
+            Translate (-100) 0 $ Scale 0.1 0.1 $ Text gameOver
+
+sampleEventHandler :: Event -> GameField -> GameField
 sampleEventHandler _ n = n
 
-sampleUpdater :: Float -> Int -> Int
-sampleUpdater _ n = n + 1
+sampleUpdater :: Float -> GameField -> GameField
+sampleUpdater delta n = n {
+    fps = 1 / delta
+}
 
 main :: IO ()
 main = play 
--- src/BreakOut/Type.hs
+++ src/BreakOut/Type.hs
@@ -1,8 +1,13 @@
-module Type where
+module BreakOut.Type where
 
 data Position = Position
     { positionX :: Float
     , positionY :: Float
+    } deriving (Show, Eq)
+
+data Segment = Segment
+    { start :: Position
+    , end :: Position
     } deriving (Show, Eq)
 
 data Scene = 
