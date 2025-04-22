41b6ffce35d911ec11fbc705454c5a5c2806e35b
hase111111 <hasehasehase61@gmail.com>
2025-04-22 11:42:12 +0900

    Update: ウィンドウを表示する処理を追加

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,20 @@ module Main where
 
 import Graphics.Gloss
 
+-- Windowのサイズを指定する関数，Num型にすることで，IntやFloatで渡すことができる.
+myWindowWidth :: Num a => a
+myWindowWidth = 960
+
+myWindowHeight :: Num a => a
+myWindowHeight = 540
+
+-- Windowのタイトルを指定する関数.
+myWindowTitle :: String
+myWindowTitle = "breakout-hs"
+
+-- Displayの引数を指定する関数. ウィンドウタイトル，横幅・縦幅，ウィンドウの位置を指定する.
+myWindowDisplay :: Display
+myWindowDisplay = InWindow myWindowTitle (myWindowWidth, myWindowHeight) (10, 10)
+
 main :: IO ()
-main = display (InWindow "rogue-hs" (320, 180) (10, 10)) white (Circle 80)
+main = display myWindowDisplay white blank
